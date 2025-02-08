55cdc0d500724b6db5ba0ce54053d8e368f051e5
macductan <macductan.19@gmail.com>
2025-02-08 21:30:41 +0700

    Update neovim installation script to include -y flag and additional dependencies

--- neovim.sh
+++ neovim.sh
@@ -1,9 +1,14 @@
 sudo apt update
 
-sudo apt install neovim
+sudo apt install neovim -y
 sudo apt install python3-neovim
-sudo apt-get install software-properties-common
+
+sudo apt-get install python-software-properties
+
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+sudo apt-get install python-dev python-pip python3-dev
+sudo apt-get install python3-setuptools
+sudo easy_install3 pip
