f75df3393ec3f7a7c3da968e19b660b38d7067b7
macductan <macductan.19@gmail.com>
2025-02-08 20:32:20 +0700

    Replace curl with wget for script downloads in README.md and index.sh

--- index.sh
+++ index.sh
@@ -1,17 +1,9 @@
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/package.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/postman.sh)
-
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh)
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/package.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/postman.sh | sh
+wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh | sh
