952b4fddbe861e61dcd098a878f89a6c2a0f815f
macductan <macductan.19@gmail.com>
2025-02-08 20:19:30 +0700

    Add indicator-multiload installation script and update index.sh to include it

--- index.sh
+++ index.sh
@@ -13,3 +13,5 @@ sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubun
 sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh)
 
 sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/postman.sh)
+
+sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh)
