5c502ac761f8b3d48d59e87c59ba3adb45427577
macductan <macductan.19@gmail.com>
2025-02-08 21:40:39 +0700

    Update neovim installation script to use software-properties-common and correct Python package names

--- neovim.sh
+++ neovim.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt install neovim -y
 sudo apt install python3-neovim
 
-sudo apt-get install python-software-properties
+sudo apt-get install software-properties-common
 
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
-sudo apt-get install python-dev python-pip python3-dev
+sudo apt-get install python-dev-is-python3 python3-pip python3-dev
 sudo apt-get install python3-setuptools
 sudo easy_install3 pip
