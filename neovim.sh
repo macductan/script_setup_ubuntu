3b727b6edcd601f3170fd55b16a474620aa247bc
macductan <macductan.19@gmail.com>
2025-02-08 21:26:02 +0700

    Replace wget with curl for script execution in index.sh and add neovim installation script

---
+++ neovim.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt install neovim
+sudo apt install python3-neovim
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install python-dev python-pip python3-dev python3-pip
