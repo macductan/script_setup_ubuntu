b3eb6e58ff97498b28cd8fbafa2b8b3942dfef48
macductan <macductan.19@gmail.com>
2025-02-08 21:48:17 +0700

    Update neovim installation script to use apt-get and include -y flag for setuptools

--- neovim.sh
+++ neovim.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt-get update
 
 sudo apt install neovim -y
 sudo apt install python3-neovim
@@ -10,5 +11,5 @@ sudo apt-get update
 sudo apt-get install neovim
 
 sudo apt-get install python-dev-is-python3 python3-pip python3-dev
-sudo apt-get install python3-setuptools
+sudo apt-get install python3-setuptools -y
 sudo easy_install3 pip
