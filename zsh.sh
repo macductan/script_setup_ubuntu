db38b378307290d7676e5d22b13769dda517e3f0
macductan <macductan.19@gmail.com>
2025-02-09 10:52:48 +0700

    Add directories for custom zsh plugins in zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,6 +5,8 @@ zsh --version
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir -p $ZSH_CUSTOM/plugins
+mkdir -p ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
