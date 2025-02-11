b5b63e13dea6c5a8f8134e7084b3beedadaccc47
macductan <macductan.19@gmail.com>
2025-02-11 15:06:37 +0700

    Remove trailing semicolons from script execution commands in index.sh for consistency

--- index.sh
+++ index.sh
@@ -1,15 +1,15 @@
-bash -c 'wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/package-default.sh | bash -x';
+bash -c 'wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/package-default.sh | bash -x'
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh)
 
-bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh);
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh)
