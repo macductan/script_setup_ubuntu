1dccbcf1a8132e0fc2c407da5ee288a16e91070e
macductan <macductan.19@gmail.com>
2025-02-11 09:16:45 +0700

    Change script execution from sh to bash for improved compatibility

--- index.sh
+++ index.sh
@@ -1,17 +1,17 @@
 wget -O - https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/package-default.sh | sh
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/zsh.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/flameshot.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/diodon.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/nvm.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/docker.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/bamboo-key.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/neovim.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/neovim.sh)
 
-sh -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh)
+bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/indicator-multiload.sh)
