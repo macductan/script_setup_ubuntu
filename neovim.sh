e0f18946eaa5fdcc081566a056fc00f2214530f4
macductan <macductan.19@gmail.com>
2025-02-11 14:37:26 +0700

    Reorganize neovim.sh to move config installation to the end for better clarity

--- neovim.sh
+++ neovim.sh
@@ -15,10 +15,6 @@ sudo apt-get install python-dev-is-python3 python3-pip python3-dev -y
 sudo apt-get install python3-setuptools -y
 sudo easy_install3 pip
 
-# Install config neovim
-mkdir -p ~/.config/nvim
-git clone git@github.com:macductan/vim-setup.git ~/.config/nvim
-
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -30,3 +26,8 @@ mkdir -p ~/.local/share/fonts
 mv JetBrainsMono ~/.local/share/fonts/
 fc-cache -fv
 rm -rf JetBrainsMono.tar.xz
+
+
+# Install config neovim
+mkdir -p ~/.config/nvim
+git clone git@github.com:macductan/vim-setup.git ~/.config/nvim
