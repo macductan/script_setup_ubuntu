ecfa159aefa57028057d2c5e1ef4d4d1ff03d19f
macductan <macductan.19@gmail.com>
2025-05-29 23:11:16 +0700

    Remove redundant bashrc updates from Homebrew installation script

--- homebrew.sh
+++ homebrew.sh
@@ -2,6 +2,4 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo >> /home/$USER/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.bashrc
-eval "$/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
