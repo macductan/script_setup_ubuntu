88a6fecc989b3ddca976d181221a2c4734e2f9f7
macductan <macductan.19@gmail.com>
2025-05-29 22:58:31 +0700

    Add Homebrew environment setup to homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -1,3 +1,7 @@
 # bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/homebrew.sh)
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /home/$USER/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.bashrc
+eval "$/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
