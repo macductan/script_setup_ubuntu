3285422e2525c9a09954e4ebc49a235d8931b2e4
macductan <macductan.19@gmail.com>
2025-05-29 22:07:10 +0700

    Refactor neovim installation script to remove unnecessary APT commands and streamline the process

--- neovim.sh
+++ neovim.sh
@@ -1,24 +1,13 @@
 # bash -x <(curl -fsSL https://raw.githubusercontent.com/macductan/script_setup_ubuntu/main/neovim.sh)
 
-sudo apt update
-sudo apt-get update
-
-# Install neovim
-sudo apt install neovim -y
-sudo apt install python3-neovim
-
-sudo apt-get install software-properties-common
-
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-brew install neovim -y
-
-sudo apt-get install python-dev-is-python3 python3-pip python3-dev -y
-sudo apt-get install python3-setuptools -y
-sudo easy_install3 pip
+brew install neovim
 
 # Install lib support
 sudo brew install rg -y
+
+# Install lazy.nvim
+git clone git@github.com:macductan/vim-setup.git ~/.config/nvim
+git -C ~/.config/nvim checkout lazy-vim
 
 # # Install vim-plug
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
