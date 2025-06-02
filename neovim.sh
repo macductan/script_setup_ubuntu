e4afa3e2b40d753cb18c35d4490421ae3875ff76
macductan <tanmd@foobla.com>
2025-06-02 16:42:18 +0700

    Refactor font installation in neovim setup: switch from wget to curl and simplify extraction process

--- neovim.sh
+++ neovim.sh
@@ -7,13 +7,14 @@ sudo apt install python3.12-venv -y
 brew install rg
 brew install jesseduffield/lazygit/lazygit
 
-# Install fonts
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
-&& cd ~/.local/share/fonts \
-&& unzip JetBrainsMono.zip \
-&& rm JetBrainsMono.zip \
-&& fc-cache -fv
-&& cd
+# Install font
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
+mkdir JetBrainsMono
+tar -xvf JetBrainsMono.tar.xz -C JetBrainsMono
+mkdir -p ~/.local/share/fonts
+mv JetBrainsMono ~/.local/share/fonts/
+fc-cache -fv
+rm -rf ./JetBrainsMono.tar.xz
 
 # Install lazy.nvim
 git clone git@github.com:macductan/vim-setup.git ~/.config/nvim
@@ -22,16 +23,6 @@ git -C ~/.config/nvim checkout lazy-vim
 # # Install vim-plug
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# # Install font
-# curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-# mkdir JetBrainsMono
-# tar -xvf JetBrainsMono.tar.xz -C JetBrainsMono
-# mkdir -p ~/.local/share/fonts
-# mv JetBrainsMono ~/.local/share/fonts/
-# fc-cache -fv
-# rm -rf ./JetBrainsMono.tar.xz
-
 
 # # Install config neovim
 # mkdir -p ~/.config/nvim
